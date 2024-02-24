51db21e33f42341dc41318646603c963e4aacd09
DouglasTakada <douglas.takada@gmail.com>
2024-02-23 18:33:31 -0800

    checking repo

--- Python-Projects/pathfinder/pathfinder.py
+++ Python-Projects/pathfinder/pathfinder.py
@@ -3,6 +3,7 @@ from curses import wrapper
 import queue
 import time
 
+# Just checking if the repos work properly....
 maze = [
     ["#", "O", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
     ["#", " ", "#", " ", " ", " ", "#", " ", " ", " ", " ", " ", " ", " ", " ", "#"],
